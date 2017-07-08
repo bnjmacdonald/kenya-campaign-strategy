1ed900f25213cf83333abab9e48b37bb87fadb18
bnjmacdonald <bnjmacdonald@gmail.com>
2017-07-08 15:55:45 -0700

    minor changes

--- bin/scrape.sh
+++ bin/scrape.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 home="~/projects" # /afs/.ir/users/b/m/bmacdon
-python3 ~/projects/kenya-campaign-strategy/module/video_scraper.py --max-results 50 --max-pages 5 --region-code KE --relevance-language sw
+python3 $home/kenya-campaign-strategy/module/video_scraper.py --max-results 50 --max-pages 5 --region-code KE --relevance-language sw
