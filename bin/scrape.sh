9ef3b6d5460003c274fc4c0618e9941d2724d1ad
Bobbie NJ Macdonald <bnjmacdonald@gmail.com>
2017-07-25 05:46:52 +0000

    added video download functionality - closes #1

--- bin/scrape.sh
+++ bin/scrape.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-home="~/projects" # /afs/.ir/users/b/m/bmacdon
+source /home/bmacdon/.virtualenvs/kenya-campaign-strategy/bin/activate
+home="/home/bmacdon/projects" # /afs/.ir/users/b/m/bmacdon
 python3 $home/kenya-campaign-strategy/module/video_scraper.py --max-results 50 --max-pages 5 --region-code KE --relevance-language sw
